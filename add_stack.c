40c711c479271dfcc12f9bb7329c7e985f356516
Wangutuai Arshton <wangarshton@gmail.com>
2023-08-25 01:16:09 +0300

    Update add_stack.c

--- add_stack.c
+++ add_stack.c
@@ -1,24 +1,16 @@
-<<<<<<< HEAD
 #include "monty.h"
-=======
-i#include "monty.h"
->>>>>>> eb2c07bce4d74d19215e0144853fd13ffb8da388
 /**
  * add - add a new node at the beginning of a list
  * @stack: a struct node pointer to the list
  * @line_number: data to enter the list
- * Return: an update linked list
+ * Return: an updated linked list
  */
 void add(stack_t **stack, unsigned int);
 {
-<<<<<<< HEAD
     stack_t *new_node;
-=======
-    stack_t *next_node;
->>>>>>> eb2c07bce4d74d19215e0144853fd13ffb8da388
     int num;
 
-    if (mont->arr[1] == NULL || !is_digit(mont->arr[1]))
+    if (mont->arr[1] == NULL || !check_digit(mont->arr[1]))
     {
         fprintf(stderr, "L %d: usage: push integer\n", line_number);
         free_stack(stack);
@@ -28,7 +20,6 @@ void add(stack_t **stack, unsigned int);
         exit(EXIT_FAILURE);
     }
     num = (int)atoi(mont->arr[1]);
-<<<<<<< HEAD
     new_node = malloc(sizeof(stack_t));
     if (!new_node)
         malloc_error();
@@ -44,32 +35,15 @@ void add(stack_t **stack, unsigned int);
         (*stack)->prev = new_node;
     }
     *stack = new_node;
-=======
-    next_node = malloc(sizeof(stack_t));
-    if (!next_node)
-        malloc_error();
-    next_node->n = num;
-    if (*stack == NULL)
-    {
-        next_node->prev = NULL;
-        next_node->next = NULL;
-    }
-    else
-    {
-        next_node->next = *stack;
-        (*stack)->prev = next_node;
-    }
-    *stack = next_node;
->>>>>>> eb2c07bce4d74d19215e0144853fd13ffb8da388
     
 }
 /**
- * is_digit - checks if the command is a digit
+ * check_digit - checks if the command is a digit
  * @c: a single pointer
  * Return: return an integer
  */
 
-int is_digit(char *c)
+int check_digit(char *c)
 {
     int i = 0;
 
@@ -86,11 +60,11 @@ int is_digit(char *c)
 }
 
 /**
- * find_entry - let program discover entry point
+ * entry_point - let program discover entry point
  * @stack: a stack node list
  * @line_number: opcode line number
  */
-void find_entry(stack_t **stack, unsigned int line_number)
+void entry_point(stack_t **stack, unsigned int line_number)
 {
     (void)stack;
     (void)line_number;
@@ -107,14 +81,10 @@ void find_entry(stack_t **stack, unsigned int line_number)
  */
 void end_stack(stack_t **stack, unsigned int line_number)
 {
-<<<<<<< HEAD
     stack_t *new_node, *current;
-=======
-    stack_t *next_node, *current;
->>>>>>> eb2c07bce4d74d19215e0144853fd13ffb8da388
     int num;
 
-    if (mont->arr[1] == NULL || !is_digit(mont->arr[1]))
+    if (mont->arr[1] == NULL || !check_digit(mont->arr[1]))
     {
         fprintf(stderr, "L%u: usage: push integer\n", line_number);
         free_stack(stack);
@@ -125,7 +95,6 @@ void end_stack(stack_t **stack, unsigned int line_number)
     }
     num = (int)atoi(mont->arr[1]);
     current = *stack;
-<<<<<<< HEAD
     new_node = malloc(sizeof(stack_t));
     if (!new_node)
         malloc_error();
@@ -134,26 +103,11 @@ void end_stack(stack_t **stack, unsigned int line_number)
     new_node->prev = NULL;
     if (*stack == NULL)
         *stack = new_node;
-=======
-    next_node = malloc(sizeof(stack_t));
-    if (!next_node)
-        malloc_error();
-    next_node->n = num;
-    next_node->next = NULL;
-    next_node->prev = NULL;
-    if (*stack == NULL)
-        *stack = next_node;
->>>>>>> eb2c07bce4d74d19215e0144853fd13ffb8da388
     else
     {
         while (current && current->next)
             current = current->next;
-<<<<<<< HEAD
         current->next = new_node;
         new_node->prev = current;
-=======
-        current->next = next_node;
-        next_node->prev = current;
->>>>>>> eb2c07bce4d74d19215e0144853fd13ffb8da388
     }
 }
