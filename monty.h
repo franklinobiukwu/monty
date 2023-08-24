65bc577eb9ef061fa02e2903dfc5ae7cd92e3d08
wang-arshton256 <wangarshton@gmail.com>
2023-08-24 14:03:46 -0700

    a monty refactor

--- monty.h
+++ monty.h
@@ -1,11 +1,17 @@
 #ifndef MONTY_H
 #define MONTY_H
+#define BUF_SIZE 1024
 
 /*LIBRARY INCLUDES*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
+#include <unistd.h>
+#include <stdarg.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 
 /*STRUCTS*/
 #include "structs.h"
@@ -20,5 +26,9 @@ void kill(const char *message);
 void free_stack(void);
 void free_arr(char **arr);
 char *error_message(char *message);
+void add_stack(stack_t **stack, unsigned int);
+void push_stack(stack_t **stack, unsigned int);
+void end_stack(stack_t **stack, unsigned int);
 
 #endif /*MONTY_H*/
+
