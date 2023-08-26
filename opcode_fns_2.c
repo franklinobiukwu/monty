17922f9c79de89dc540fb989d91ae7be6b053861
Franklin Obiukwu <obiukwuchibuisi@gmail.com>
2023-08-26 14:53:33 +0100

    Implement the add opcode function

--- opcode_fns_2.c
+++ opcode_fns_2.c
@@ -1,4 +1,6 @@
 #include "monty.h"
+#include "structs.h"
+#include <stdio.h>
 
 
 /**
@@ -37,7 +39,19 @@ void _swap(__attribute((unused))stack_t **stack,
 void _add(__attribute((unused))stack_t **stack,
 	   __attribute((unused))unsigned int line_number)
 {
-	printf("Add t nodes");
+	stack_t *head = gv->head;
+
+	/*If stack is NULL of stack elements are less than 2*/
+	if (head == NULL || head->next == NULL)
+	{
+		fprintf(stderr, "L%d: can't add, stack too short",
+	  gv->line_number);
+		kill(NULL);
+	}
+	/*sum values of first two node of list and store in second node*/
+	gv->head->next->n = head->n + head->next->n;
+	/*remove top element*/
+	_pop(&(gv->head), gv->line_number);
 }
 
 /**
